3a8a6408b289956be4682ab8e959c54a59c11fdf
Trail <82917438+zakyislm@users.noreply.github.com>
2025-07-23 15:32:40 +0700

    added few things
    especially on the web detailings

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,9 @@
-const konektor = document.querySelector('.konektor_website');
-const koneksi = document.querySelector('.koneksi_website');
-
-let timer;
-
-function showKoneksi() {
-  clearTimeout(timer);
-  koneksi.style.display = 'block';
-}
-
-function hideKoneksi() {
-  timer = setTimeout(() => {
-    koneksi.style.display = 'none';
-  }, 200); // delay dikit biar smooth
-}
-
-konektor.addEventListener('mouseenter', showKoneksi);
-konektor.addEventListener('mouseleave', hideKoneksi);
-koneksi.addEventListener('mouseenter', showKoneksi);
-koneksi.addEventListener('mouseleave', hideKoneksi);
+// document.getElementById('tombol').addEventListener('hover', function () {
+//   var target = document.getElementById('target');
+  
+//   if (target.classList.display='none') {
+//       target.classList.display='block';
+//   } else {
+//     target.classList.display='none';
+//   }
+// });
